27ce07b2211f33b7944d147ef47573b12f0461a1
Elias Misael Valdez Faria <evaldez@cerouno.com.py>
2022-01-12 12:27:24 -0300

    Precios added

--- src/components/NuestrosProductos.js
+++ src/components/NuestrosProductos.js
@@ -1,7 +1,51 @@
+import React from "react";
+import "../styles/pages/NuestrosProductos.css";
+import pizza from "../assets/pizza.jpg";
+import hamburguesa from "../assets/hamburguer2.jpg";
+import margarita from "../assets/pizza-cabarecco3.jpg";
+
 const NuestrosProductos = () => {
   return (
-    <div id="NuestrosProductos" className="h2-container">
-      <h2>Productos</h2>
+    <div id="NuestrosProductos" className="nuestros_productos">
+      <h2 className="nuestros_productos-headline">Nuestros Productos más populares</h2>
+      <div className="nuestros_productos-cardContainer">
+        <div className="nuestros_productos-card">
+          <img className="nuestros_productos-img" src={pizza} alt="pizza"/>
+          <div className="nuestros_productos-itemsInsideCard">
+            <p>⭐⭐⭐⭐⭐</p>
+            <h2>Pepperoni</h2>
+            <p className="nuestros_productos-card-p">Queso muzzarella, pepperoni oregano, salsa de tomate</p>
+            <div className="nuestros_productos-price">
+              <h2>30.000 Gs.</h2>
+              <div className="nuestros_productos-heart">♡</div>
+            </div>
+          </div>
+        </div>
+        <div className="nuestros_productos-card">
+          <img className="nuestros_productos-img" src={hamburguesa} alt="pizza"/>
+          <div className="nuestros_productos-itemsInsideCard">
+            <p>⭐⭐⭐⭐⭐</p>
+            <h2>Hamburguesa</h2>
+            <p className="nuestros_productos-card-p">Pan, lechuga, tomate, carne, cheddar, cebolla, bacon</p>
+            <div className="nuestros_productos-price">
+              <h2>30.000 Gs.</h2>
+              <div className="nuestros_productos-heart">♡</div>
+            </div>
+          </div>
+        </div>
+        <div className="nuestros_productos-card">
+          <img className="nuestros_productos-img" src={margarita} alt="pizza"/>
+          <div className="nuestros_productos-itemsInsideCard">
+            <p>⭐⭐⭐⭐⭐</p>
+            <h2>Margarita clásica</h2>
+            <p className="nuestros_productos-card-p">Queso muzzarella, oregano, salsa de tomate</p>
+            <div className="nuestros_productos-price">
+              <h2>30.000 Gs.</h2>
+              <div className="nuestros_productos-heart">♡</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Precios.js
+++ src/components/Precios.js
@@ -1,7 +1,17 @@
+import React from "react";
+import "../styles/pages/Precios.css";
+
 const Precios = () => {
   return (
-    <div id="Precios" className="h2-container">
-      <h2>Precios</h2>
+    <div id="Precios" className="precios_container">
+      <h2 className="precios_container-headline">Nuestras especialidades</h2>
+      <div className="precios_container-cards">
+        <div className="precios_container-yellowCard">0</div>
+        <div className="precios_container-cardOne">1</div>
+        <div className="precios_container-cardTwo">2</div>
+        <div className="precios_container-cardThree">3</div>
+        <div className="precios_container-cardFour">4</div>
+      </div>
     </div>
   );
 }
